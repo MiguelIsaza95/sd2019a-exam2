0942ce55a211ff671dfc14f4c761d582b0809474
Marisol Giraldo <marisol.giraldo@correo.icesi.edu.co>
2019-04-24 20:15:24 -0500

    Add Files

---
+++ app/app.py
@@ -0,0 +1,49 @@
+from typing import List, Dict
+from flask import Flask
+import mysql.connector
+import json
+import logging
+import random
+
+app = Flask(__name__)
+
+
+def favorite_colors() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'knights'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM favorite_colors')
+    results = [{name: color} for (name, color) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+@app.route('/')
+def index() -> str:
+    return json.dumps({'favorite_colors': favorite_colors()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+
+logging.basicConfig(filename="logFile.txt",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+for i in xrange(0,15):
+    x=random.randint(0,2)
+    if(x==0):
+            logging.warning('Log Message')
+    elif(x==1):
+            logging.critical('Log Message')
+    else:
+            logging.error('Log Message')
